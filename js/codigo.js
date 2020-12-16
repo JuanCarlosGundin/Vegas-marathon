79c5a2f3c649317ad8e3e01347abf013eff521f0
JuanCarlosGundin <juancarlosgr9898@gmail.com>
2020-12-16 13:29:15 +0100

    Subida definitiva

--- js/codigo.js
+++ js/codigo.js
@@ -1,19 +1,3 @@
-window.onscroll = function() {
-    scrollFunction()
-};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("nav").style.backgroundColor = "rgba(255,255,255,0.5";
-        // document.getElementById("nav").style.backdropFilter = "blur(4px)";
-        //document.getElementById("nav").style.backdropFilter = "sepia(0.9)";
-    } else {
-        document.getElementById("nav").style.backgroundColor = "rgba(255,255,255,0.5)";
-        // document.getElementById("nav").style.backdropFilter = "blur(0px)";
-        //document.getElementById("nav").style.backdropFilter = "sepia(0.9)";
-    }
-};
-
 function startTimer(duration, display) {
     var timer = duration,
         minutes, seconds;
